7043354b736b69d1bfc50e7c22740509f6501fc1
jnnilsson <jn.jensnilsson@gmail.com>
2023-12-05 16:33:45 +0100

    create connection

--- src/main/java/org/example/Labb3JensNilssonJU23.java
+++ src/main/java/org/example/Labb3JensNilssonJU23.java
@@ -1,7 +1,22 @@
 package org.example;
 
-public class Labb3JensNilssonJU23 {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Labb3JensNilssonJU23 {
+
+    private static Connection connect() {
+        String url = "jdbc:sqlite:/Users/jensnilsson/Documents/sqlite- filer/SQliteLabb3JensNilssonJU23.db";
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
     }
 }
