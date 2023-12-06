913d4bd94089a8f766b714dda4673d42a8ea19c8
jnnilsson <jn.jensnilsson@gmail.com>
2023-12-06 12:11:53 +0100

    added menu, add switch for user options

--- src/main/java/org/example/Labb3JensNilssonJU23.java
+++ src/main/java/org/example/Labb3JensNilssonJU23.java
@@ -3,8 +3,11 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Labb3JensNilssonJU23 {
+
+    private static Scanner scanner = new Scanner(System.in);
 
     private static Connection connect() {
         String url = "jdbc:sqlite:/Users/jensnilsson/Documents/sqlite- filer/SQliteLabb3JensNilssonJU23.db";
@@ -17,6 +20,35 @@ public class Labb3JensNilssonJU23 {
         return connection;
     }
 
+    private static void printMenuOptions() {
+        System.out.print("""
+                Choose an option:
+                =============
+                0. Exit the program
+                1. Show all movies
+                2. Add a new movie
+                3. Update a existing movie
+                4. Remove a movie
+                5. Show all menu options
+                """);
+    }
+
+    private static void showAllMovies() {
+    }
+
     public static void main(String[] args) {
+        String selection;
+        do {
+            printMenuOptions();
+            selection = scanner.next();
+            try {
+                switch (selection) {
+                    case "0" -> System.out.println("Exiting the program...");
+                    case "1" -> showAllMovies();
+                }
+            } catch (IndexOutOfBoundsException exception) {
+                System.out.println("Choose the right option...");
+            }
+        } while (!selection.equals("0"));
     }
 }
